72e9a3a002c3e5f7882c7b8c826cfd8dc8791eda
Deboh12 <deborah.alexander12@yahoo.com>
2024-01-20 21:49:48 -0600

    refined my seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,27 @@
 const sequelize = require('../config/connection');
 const { User, Post } = require('../models');
+
 const userData = require('./userData.json');
 const postData = require('./postData.json');
 
 const seedDatabase = async () => {
-    await sequelize.sync({ force: true });
-    await User.bulkCreate(userData, {
+    await sequelize.sync({ force: true }); // This will reset your database
+
+    // Seed Users
+    const users = await User.bulkCreate(userData, {
         individualHooks: true,
         returning: true,
     });
-    await Post.bulkCreate(postData);
+
+    // Seed Posts
+    for (const post of postData) {
+        await Post.create({
+            ...post,
+            // Assuming your postData.json doesn't include user_ids, assign them here
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+    }
+
     process.exit(0);
 };
 
