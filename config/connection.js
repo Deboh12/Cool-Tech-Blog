8973b6e77e8e6fcd736adbd45c7526b22b58c490
Deboh12 <deborah.alexander12@yahoo.com>
2024-01-21 16:04:10 -0600

    Update database configuration for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,24 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, 
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306,
-    logging: false 
-  }
-);
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    // Use JawsDB MySQL database on Heroku
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+ 
+    sequelize = new Sequelize(
+        process.env.DB_NAME, 
+        process.env.DB_USER, 
+        process.env.DB_PASSWORD, 
+        {
+            host: process.env.DB_HOST,
+            dialect: 'mysql',
+            port: process.env.DB_PORT || 3306,
+            logging: false  
+        }
+    );
+}
 
 module.exports = sequelize;
